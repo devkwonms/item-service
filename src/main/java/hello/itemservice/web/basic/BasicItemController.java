2816492cddce53310ebc356832f47810db39ef1c
devkwonms <alstj1379@nate.com>
2023-01-06 17:24:15 +0900

    BasicItemController - 컨트롤러 로직은 itemRepository에서 모든 상품을 조회한 다음에 모델에 담고, 뷰 템플릿을 호출함

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,42 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+
+    @GetMapping
+    public String item(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * test용 데이터 추가
+     */
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA",10000,10));
+        itemRepository.save(new Item("itemB",20000,20));
+    }
+}
