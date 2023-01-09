24f77189c78aa7ac0f5106b5ea4409863462ad1b
devkwonms <alstj1379@nate.com>
2023-01-09 13:16:39 +0900

    addItemV1 - BasicItemController에 추가

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,10 +5,7 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -40,6 +37,30 @@ public class BasicItemController {
         System.out.println("itemId = " + itemId);
         return "basic/item";
     }
+
+    @GetMapping("/add")
+    public String addForm() {
+
+        return "/basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+
+        return "/basic/item";
+    }
+
 
     /**
      * test용 데이터 추가
